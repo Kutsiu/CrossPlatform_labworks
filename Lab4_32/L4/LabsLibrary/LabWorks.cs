f26b9c6e57a609473f75c08025c03988a104d4d2
Nikita <Nikkut01@gmail.com>
2023-10-22 21:58:46 +0300

    added files

---
+++ Lab4_32/L4/LabsLibrary/LabWorks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsLibrary
+{
+    public static class LabWorks
+    {
+        public static void Execute(string labName)
+        {
+            var path = Environment.GetEnvironmentVariable("LAB_PATH");
+            if (path == null)
+            {
+                Console.WriteLine("Path is incorrect!");
+                return;
+            }
+            switch (labName.ToLower())
+            {
+                case "lab1":
+                    L1.Exec(path);
+                    break;
+                case "lab2":
+                    L2.Exec(path);
+                    break;
+                case "lab3":
+                    L3.Exec(path);
+                    break;
+                default:
+                    Console.WriteLine("Unknown lab!");
+                    break;
+            }
+        }
+    }
+}
